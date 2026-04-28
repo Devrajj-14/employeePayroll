fa2305c5a2ecebcf479e98031f5dd712842fb3e6
Devraj <devraj@example.com>
2026-04-29 01:41:53 +0530

    [Devraj]: complete UC4 feature - Routing and Global CSS

---
+++ src/home/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './home.scss';
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1>Hello from BridgeLabz</h1>
+      <p>Welcome to Employee Payroll Application</p>
+    </div>
+  );
+};
+
+export default Home;
